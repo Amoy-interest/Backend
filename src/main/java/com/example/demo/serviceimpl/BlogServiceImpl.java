6fbabaac8740c3e28b38cbb9a3e3d6a09ea0dd1f
explodingnerk <wuzechuan@sjtu.edu.cn>
2020-07-13 16:38:49 +0800

    feat: add blog module

--- src/main/java/com/example/demo/serviceimpl/BlogServiceImpl.java
+++ src/main/java/com/example/demo/serviceimpl/BlogServiceImpl.java
@@ -1,10 +1,34 @@
 package com.example.demo.serviceimpl;
 
 import com.example.demo.dao.BlogDao;
+import com.example.demo.entity.Blog;
 import com.example.demo.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BlogServiceImpl implements BlogService {
+
     @Autowired
-    BlogDao blogDao;
+    private BlogDao blogDao;
+
+    @Override
+    public Blog addBlog(Blog blog) {
+        return blogDao.saveBlog(blog);
+    }
+
+    @Override
+    public Blog updateBlog(Blog blog) {
+        return blogDao.saveBlog(blog);
+    }
+
+    @Override
+    public Blog findBlogByBlog_id(Integer blog_id) {
+        return blogDao.findBlogByBlog_id(blog_id);
+    }
+
+    @Override
+    public void deleteByBlog_id(Integer blog_id) {
+        blogDao.deleteByBlog_id(blog_id);
+    }
 }
