3460744b263d27fc324c40e480b49f8981e3733e
explodingnerk <wuzechuan@sjtu.edu.cn>
2020-07-15 14:24:33 +0800

    feat: blogControllerTest v1

--- src/test/java/com/example/demo/jUnit5Test/BlogControllerTest.java
+++ src/test/java/com/example/demo/jUnit5Test/BlogControllerTest.java
@@ -3,6 +3,7 @@ package com.example.demo.jUnit5Test;
 import com.example.demo.DemoApplicationTests;
 import com.example.demo.controller.BlogController;
 import com.example.demo.entity.Blog;
+import com.example.demo.entity.BlogComment;
 import com.example.demo.serviceimpl.BlogServiceImpl;
 import net.sf.json.JSONObject;
 import org.junit.Before;
@@ -19,6 +20,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -78,6 +82,7 @@ public class BlogControllerTest extends DemoApplicationTests {
 
     @Test
     public void testDeleteBlog() throws Exception {
+        doNothing().when(blogService).deleteByBlog_id(Mockito.any());
         mockMvc.perform(delete("/blog?blog_id=1")
                 .header("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3R5cGUiOjAsInVzZXJfaWQiOjEsImlzcyI6ImF1dGgwIiwiZXhwIjoxNTk1NjQ2OTQyfQ.8Ycii-oG6JtxOO1DGTqdAJV1FOUWpvEJyYOTCBc06Us"))
                 .andExpect(status().isOk()).andReturn();
@@ -86,15 +91,37 @@ public class BlogControllerTest extends DemoApplicationTests {
 
     @Test
     public void testVote() throws Exception {
+        doNothing().when(blogService).incrVoteCount(Mockito.any());
+        doNothing().when(blogService).incrCommentVoteCount(Mockito.any());
         mockMvc.perform(post("/blog/vote?blog_id=1&comment_id=-1")
                 .header("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3R5cGUiOjAsInVzZXJfaWQiOjEsImlzcyI6ImF1dGgwIiwiZXhwIjoxNTk1NjQ2OTQyfQ.8Ycii-oG6JtxOO1DGTqdAJV1FOUWpvEJyYOTCBc06Us"))
                 .andExpect(status().isOk()).andReturn();
         verify(blogService, times(1)).incrVoteCount(Mockito.any());
-        verify(blogService, times(0)).incrCommentVoteCount(Mockito.any());
         mockMvc.perform(post("/blog/vote?blog_id=1&comment_id=1")
                 .header("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3R5cGUiOjAsInVzZXJfaWQiOjEsImlzcyI6ImF1dGgwIiwiZXhwIjoxNTk1NjQ2OTQyfQ.8Ycii-oG6JtxOO1DGTqdAJV1FOUWpvEJyYOTCBc06Us"))
                 .andExpect(status().isOk()).andReturn();
-
+        verify(blogService, times(1)).incrCommentVoteCount(Mockito.any());
     }
 
+    @Test
+    public void testComment() throws Exception {
+        Mockito.when(blogService.addBlogComment(Mockito.any())).thenReturn(null);
+        mockMvc.perform(post("/blog/comment?blog_id=1&reply_comment_username=test&root_comment_id=1&text=%E6%98%A8%E6%99%9A%E5%8D%95%E6%89%93%E5%8F%88%E8%BE%93%E4%BA%86")
+                .header("token", "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX3R5cGUiOjAsInVzZXJfaWQiOjEsImlzcyI6ImF1dGgwIiwiZXhwIjoxNTk1NjQ2OTQyfQ.8Ycii-oG6JtxOO1DGTqdAJV1FOUWpvEJyYOTCBc06Us"))
+                .andExpect(status().isOk()).andReturn();
+        verify(blogService, times(1)).addBlogComment(Mockito.any());
+    }
+
+    @Test
+    public void testSearch() throws Exception {
+        List<Blog> blogList = new ArrayList<>();
+        blogList.add(new Blog(1, 1, 0, 0,null, "abbcdde",  false, 1, -1));
+        blogList.add(new Blog(2, 1, 0, 0,null, "abcdde",  false, 1, -1));
+        Mockito.when(blogService.getAllBlogs()).thenReturn(blogList);
+        Mockito.when(blogService.getSimpleBlogDetail(1)).thenReturn(null);
+        mockMvc.perform(get("/blog/search?keyword=abbc"))
+                .andExpect(status().isOk()).andReturn();
+        verify(blogService, times(1)).getAllBlogs();
+        verify(blogService, times(1)).getSimpleBlogDetail(Mockito.any());
+    }
 }
