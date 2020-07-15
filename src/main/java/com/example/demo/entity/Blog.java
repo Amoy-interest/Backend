cf77f2c7d3787206df30a7ab08412da4e75b5085
explodingnerk <wuzechuan@sjtu.edu.cn>
2020-07-15 08:55:21 +0800

    feat: init Blog unit test

--- src/main/java/com/example/demo/entity/Blog.java
+++ src/main/java/com/example/demo/entity/Blog.java
@@ -22,6 +22,21 @@ public class Blog {
     private boolean is_deleted;
     private Integer check_status;
     private Integer reply_blog_id;
+
+    public Blog(){}
+
+    public Blog(int blog_id, int user_id, int topic_id, int blog_type, Date blog_time, String blog_text, boolean is_deleted, Integer check_status, Integer reply_blog_id) {
+        this.blog_id = blog_id;
+        this.user_id = user_id;
+        this.topic_id = topic_id;
+        this.blog_type = blog_type;
+        this.blog_time = blog_time;
+        this.blog_text = blog_text;
+        this.is_deleted = is_deleted;
+        this.check_status = check_status;
+        this.reply_blog_id = reply_blog_id;
+    }
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="user_id",insertable = false, updatable = false)
     private User user;
@@ -41,4 +56,5 @@ public class Blog {
     @OneToMany(fetch=FetchType.LAZY)
     @JoinColumn(name = "blog_id",referencedColumnName = "blog_id")
     private List<BlogImage> blogImages;
+
 }
