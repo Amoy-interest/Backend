7e0f222f1cf4138945f0274352576ebbaf6a4438
白泽 <mokkkkk@sjtu.edu.cn>
2020-07-22 17:58:12 +0800

    fix: 转发评论返回参数加上用户头像，登录返回user_id

--- src/main/java/com/example/amoy_interest/dto/BlogCommentLevel1DTO.java
+++ src/main/java/com/example/amoy_interest/dto/BlogCommentLevel1DTO.java
@@ -21,6 +21,8 @@ public class BlogCommentLevel1DTO {
     private Integer user_id;
     @ApiModelProperty(value = "该评论的用户名", required = true)
     private String nickname;
+    @ApiModelProperty(value = "该评论的用户头像", required = true)
+    private String avatar_path;
     @ApiModelProperty(value = "该评论的评论id", required = true)
     private Integer comment_id;
 //    @ApiModelProperty(value = "该评论所在的博文id", required = true)
@@ -34,13 +36,14 @@ public class BlogCommentLevel1DTO {
     @ApiModelProperty(value = "是否有子评论")
     private boolean have_child;
 
-    public BlogCommentLevel1DTO(BlogComment blogComment,String nickname,boolean have_child) {
+    public BlogCommentLevel1DTO(BlogComment blogComment,String nickname,boolean have_child,String avatar_path) {
         this.user_id = blogComment.getUser_id();
         this.comment_id = blogComment.getComment_id();
         this.comment_text = blogComment.getComment_text();
         this.comment_time = blogComment.getComment_time();
         this.vote_count = blogComment.getVote_count();
         this.nickname = nickname;
+        this.avatar_path = avatar_path;
         this.have_child = have_child;
     }
 }
