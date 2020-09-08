2da8485ffc2e807dc3348b6232f2da799eab65cf
explodingnerk <wuzechuan@sjtu.edu.cn>
2020-09-08 09:58:33 +0800

    fix: merge

--- src/main/java/com/example/amoy_interest/config/QuartzConfig.java
+++ src/main/java/com/example/amoy_interest/config/QuartzConfig.java
@@ -19,70 +19,70 @@ public class QuartzConfig {
     private static final String BLOG_HEAT_TASK_IDENTITY = "BlogHeatQuartz";
     private static final String BLOG_HEAT_TASK_2_IDENTITY = "BlogHeat2Quartz";
     private static final String TOPIC_HEAT_TASL_IDENTITY = "TopicHeatQuartz";
-//    @Bean
-//    public JobDetail jobDetail1() {
-//        return JobBuilder.newJob(CountTask.class).withIdentity(COUNT_TASK_IDENTITY).storeDurably().build();
-//    }
-//
-//    @Bean
-//    public JobDetail jobDetail2() {
-//        return JobBuilder.newJob(BlogHeatTask.class).withIdentity(BLOG_HEAT_TASK_IDENTITY).storeDurably().build();
-//    }
-//
-//    @Bean
-//    public JobDetail jobDetail3() {
-//        return JobBuilder.newJob(BlogHeatTask2.class).withIdentity(BLOG_HEAT_TASK_2_IDENTITY).storeDurably().build();
-//    }
-//
-//    @Bean
-//    public JobDetail jobDetail4() {
-//        return JobBuilder.newJob(TopicHeatTask.class).withIdentity(TOPIC_HEAT_TASL_IDENTITY).storeDurably().build();
-//    }
-//
-//    @Bean
-//    public Trigger quartzTrigger1() {
-//        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
-////                .withIntervalInSeconds(10)  //设置时间周期单位秒
-//                .withIntervalInHours(2)  //两个小时执行一次
-//                .repeatForever();
-//        return TriggerBuilder.newTrigger().forJob(jobDetail1())
-//                .withIdentity(COUNT_TASK_IDENTITY)
-//                .withSchedule(scheduleBuilder)
-//                .build();
-//    }
-//
-//    @Bean
-//    public Trigger quartzTrigger2() {
-//        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
-////                .withIntervalInSeconds(10)  //设置时间周期单位秒
-//                .withIntervalInHours(1)  //一个小时执行一次
-//                .repeatForever();
-//        return TriggerBuilder.newTrigger().forJob(jobDetail2())
-//                .withIdentity(BLOG_HEAT_TASK_IDENTITY)
-//                .withSchedule(scheduleBuilder)
-//                .build();
-//    }
-//
-//    @Bean
-//    public Trigger quartzTrigger3() {
-//        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
-////                .withIntervalInSeconds(10)  //设置时间周期单位秒
-//                .withIntervalInHours(24)  //每天执行一次
-//                .repeatForever();
-//        return TriggerBuilder.newTrigger().forJob(jobDetail3())
-//                .withIdentity(BLOG_HEAT_TASK_2_IDENTITY)
-//                .withSchedule(scheduleBuilder)
-//                .build();
-//    }
-//
-//    @Bean
-//    public Trigger quartzTrigger4() {
-//        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule().withIntervalInHours(1).repeatForever();
-//        return TriggerBuilder.newTrigger().forJob(jobDetail4())
-//                .withIdentity(TOPIC_HEAT_TASL_IDENTITY)
-//                .withSchedule(scheduleBuilder)
-//                .build();
-//    }
-//
+    @Bean
+    public JobDetail jobDetail1() {
+        return JobBuilder.newJob(CountTask.class).withIdentity(COUNT_TASK_IDENTITY).storeDurably().build();
+    }
+
+    @Bean
+    public JobDetail jobDetail2() {
+        return JobBuilder.newJob(BlogHeatTask.class).withIdentity(BLOG_HEAT_TASK_IDENTITY).storeDurably().build();
+    }
+
+    @Bean
+    public JobDetail jobDetail3() {
+        return JobBuilder.newJob(BlogHeatTask2.class).withIdentity(BLOG_HEAT_TASK_2_IDENTITY).storeDurably().build();
+    }
+
+    @Bean
+    public JobDetail jobDetail4() {
+        return JobBuilder.newJob(TopicHeatTask.class).withIdentity(TOPIC_HEAT_TASL_IDENTITY).storeDurably().build();
+    }
+
+    @Bean
+    public Trigger quartzTrigger1() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+//                .withIntervalInSeconds(10)  //设置时间周期单位秒
+                .withIntervalInHours(2)  //两个小时执行一次
+                .repeatForever();
+        return TriggerBuilder.newTrigger().forJob(jobDetail1())
+                .withIdentity(COUNT_TASK_IDENTITY)
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+
+    @Bean
+    public Trigger quartzTrigger2() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+//                .withIntervalInSeconds(10)  //设置时间周期单位秒
+                .withIntervalInHours(1)  //一个小时执行一次
+                .repeatForever();
+        return TriggerBuilder.newTrigger().forJob(jobDetail2())
+                .withIdentity(BLOG_HEAT_TASK_IDENTITY)
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+
+    @Bean
+    public Trigger quartzTrigger3() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule()
+//                .withIntervalInSeconds(10)  //设置时间周期单位秒
+                .withIntervalInHours(24)  //每天执行一次
+                .repeatForever();
+        return TriggerBuilder.newTrigger().forJob(jobDetail3())
+                .withIdentity(BLOG_HEAT_TASK_2_IDENTITY)
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+
+    @Bean
+    public Trigger quartzTrigger4() {
+        SimpleScheduleBuilder scheduleBuilder = SimpleScheduleBuilder.simpleSchedule().withIntervalInHours(1).repeatForever();
+        return TriggerBuilder.newTrigger().forJob(jobDetail4())
+                .withIdentity(TOPIC_HEAT_TASL_IDENTITY)
+                .withSchedule(scheduleBuilder)
+                .build();
+    }
+
 
 }
